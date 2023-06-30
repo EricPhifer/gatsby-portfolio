81e23a6bdc644e5ab117808b6b50170a37f7fd85
Eric Phifer <ericoncode@gmail.com>
2023-06-30 15:53:46 -0600

    update calendly link

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import styled from 'styled-components';
-import useForm from '../utils/useForm';
-import useContact from '../utils/useContact';
+import React from 'react'
+import styled from 'styled-components'
+import useForm from '../utils/useForm'
+import useContact from '../utils/useContact'
 
 const Form = styled.form`
   width: 80vw;
@@ -9,11 +9,12 @@ const Form = styled.form`
   left: 50%;
   margin-top: 8rem;
   margin-left: -40vw;
-  transition: .5s all ease;
+  transition: 0.5s all ease;
   label {
     display: none;
   }
-  input, textarea {
+  input,
+  textarea {
     font-size: 1.75rem;
   }
   legend {
@@ -42,7 +43,7 @@ const Form = styled.form`
       font-size: 2rem;
     }
   }
-`;
+`
 
 const InlineField = styled.fieldset`
   width: 98%;
@@ -56,7 +57,7 @@ const InlineField = styled.fieldset`
     padding: 1rem;
     margin: 0 0.5rem;
     border: none;
-    border-bottom: .4rem inset var(--black);
+    border-bottom: 0.4rem inset var(--black);
     background: transparent;
   }
   @media only screen and (max-width: 900px) {
@@ -72,12 +73,13 @@ const InlineField = styled.fieldset`
     input {
       font-size: 1.25rem;
     }
-    a, button {
+    a,
+    button {
       font-size: 1.5rem;
       margin-bottom: 1rem;
     }
   }
-`;
+`
 
 const FullField = styled.fieldset`
   display: flex;
@@ -89,7 +91,7 @@ const FullField = styled.fieldset`
     padding: 1rem;
     margin: 0 0.5rem;
     border: none;
-    border-bottom: .4rem inset var(--black);
+    border-bottom: 0.4rem inset var(--black);
     background: transparent;
   }
   textarea {
@@ -97,22 +99,24 @@ const FullField = styled.fieldset`
     margin-left: 0.5rem;
     padding: 1rem;
     border: none;
-    border-bottom: .4rem inset var(--black);
-    border-right: .4rem inset var(--black);
+    border-bottom: 0.4rem inset var(--black);
+    border-right: 0.4rem inset var(--black);
     background: transparent;
   }
   @media only screen and (max-width: 900px) {
-    textarea, input {
+    textarea,
+    input {
       width: 90%;
     }
   }
   @media only screen and (max-height: 750px) {
     margin-bottom: 1rem;
-    input, textarea {
+    input,
+    textarea {
       font-size: 1.25rem;
     }
   }
-`;
+`
 
 const Submit = styled.button`
   width: 20rem;
@@ -121,13 +125,13 @@ const Submit = styled.button`
   border-radius: 1rem;
   font-size: 1.85rem;
   &:focus {
-    border: .2rem dotted blue;
+    border: 0.2rem dotted blue;
   }
   @media only screen and (max-width: 900px) {
     width: 80%;
     margin-bottom: 2rem;
   }
-`;
+`
 
 const Buttonesque = styled.a`
   width: 20rem;
@@ -140,14 +144,14 @@ const Buttonesque = styled.a`
   text-align: center;
   &:hover {
     background: transparent;
-    border: .125rem solid var(--green);
+    border: 0.125rem solid var(--green);
     color: var(--green);
   }
   @media only screen and (max-width: 900px) {
     margin-right: 0;
     margin-bottom: 2rem;
   }
-`;
+`
 
 export default function ContactForm() {
   const { values, updateValue } = useForm({
@@ -155,83 +159,82 @@ export default function ContactForm() {
     email: '',
     website: '',
     message: '',
-  });
+  })
   const { contact, error, loading, errMessage, submitContact } = useContact({
     values,
-  });
-  console.log(contact, error, loading, submitContact);
+  })
+  console.log(contact, error, loading, submitContact)
   if (errMessage) {
-    return <p>{errMessage}</p>;
+    return <p>{errMessage}</p>
   }
   return (
-    <>
-      <Form
-        method='post'
-        netlify-honeypot='bot-field'
-        data-netlify='true'
-        name='standard-contact'
-      >
-        <input type="hidden" name="bot-field" />
-        <input type="hidden" name="form-name" value="contact" />
-        <legend>Contact Me</legend>
-        <InlineField className="nameEmail">
-          <label htmlFor="name">Name</label>
-          <input 
-            type="text"
-            name="name"
-            id="name"
-            value={values.name}
-            onChange={updateValue}
-            placeholder="Name"
-            className="required"
-          />
-          <label htmlFor="email">Email</label>
-          <input 
-            type="text"
-            name="email"
-            id="email"
-            value={values.email}
-            onChange={updateValue}
-            placeholder="Email"
-            className="required"
-          />
-        </InlineField>
-        <FullField className="website">
-          <label htmlFor="website">Website</label>
-          <input 
-            type="text"
-            name="website"
-            id="website"
-            value={values.website}
-            onChange={updateValue}
-            placeholder="Website Address"
-            className="required"
-          />
-        </FullField>
-        <FullField className="message">
-          <label htmlFor="message">Message</label>
-          <textarea 
-            name="message"
-            id="message"
-            value={values.message}
-            onChange={updateValue}
-            rows="7"
-            placeholder="What is your #1 need?"
-            className="required"
-          />
-        </FullField>
-        <InlineField className="buttons">
-          <Buttonesque 
-            href="https://calendly.com/ericphifer/30-minute-website-consultation" rel="noopener noreferrer" 
-            target="_blank"
-          >
-            Get a Consultation
-          </Buttonesque>
-          <Submit type="submit" value="Submit">
-            Submit
-          </Submit>
-        </InlineField>
-      </Form>
-    </>
-  );
+    <Form
+      method="post"
+      netlify-honeypot="bot-field"
+      data-netlify="true"
+      name="standard-contact"
+    >
+      <input type="hidden" name="bot-field" />
+      <input type="hidden" name="form-name" value="contact" />
+      <legend>Contact Me</legend>
+      <InlineField className="nameEmail">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          value={values.name}
+          onChange={updateValue}
+          placeholder="Name"
+          className="required"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={values.email}
+          onChange={updateValue}
+          placeholder="Email"
+          className="required"
+        />
+      </InlineField>
+      <FullField className="website">
+        <label htmlFor="website">Website</label>
+        <input
+          type="text"
+          name="website"
+          id="website"
+          value={values.website}
+          onChange={updateValue}
+          placeholder="Website Address"
+          className="required"
+        />
+      </FullField>
+      <FullField className="message">
+        <label htmlFor="message">Message</label>
+        <textarea
+          name="message"
+          id="message"
+          value={values.message}
+          onChange={updateValue}
+          rows="7"
+          placeholder="What is your #1 need?"
+          className="required"
+        />
+      </FullField>
+      <InlineField className="buttons">
+        <Buttonesque
+          href="https://calendly.com/ericphifer/60-minute-website-consultation"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          Get a Consultation
+        </Buttonesque>
+        <Submit type="submit" value="Submit">
+          Submit
+        </Submit>
+      </InlineField>
+    </Form>
+  )
 }
